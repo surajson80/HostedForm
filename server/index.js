a03eb12e643ac14a632ddce3c80c2cbba16413c3
surajson80 <surajson80@gmail.com>
2022-12-07 13:21:36 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-db.sequelize.sync();
+// db.sequelize.sync();
 require("./database/routes/transaction.route")(app)
 
 app.post('/gateway/webhook', async(req, res) =>{
